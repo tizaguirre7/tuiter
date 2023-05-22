adcaec6fba12a7928be6ecb8c90f993c1ad71e26
Tomas Izaguirre <teizaguirre331@gmail.com>
2023-05-22 16:29:05 +0100

    segundo commit con primeros cambios de pagina

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head"
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,6 +13,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Head>
+          <title>Tuiter</title>
+          <meta name="description" content="this is my twitter clone"/>
+          <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
